35d439a8e485e1d52be1c3258f0e17a0f2e20b16
Dion Snoeijen <info@diovisuals.com>
2016-07-19 14:19:15 +0200

    Updated and moved deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+green=$(tput setaf 2)
+red=$(tput setaf 1)
+yellow=$(tput setaf 3)
+dim=$(tput dim)
+
+echo "${green}"
+echo "Shutdown composer"
+
 docker-compose down
+
+echo "Pull latest images"
 
 docker images
 docker pull dionsnoeijen/blog:latest
@@ -8,7 +20,13 @@ docker pull dionsnoeijen/php_fpm:latest
 docker pull composer/composer:1.0-alpine
 docker images
 
-cd ../../..
+echo "Start containers"
+
 docker-compose up
+
+echo "Run composer"
+
 cd docker/blog/app
 docker run --rm -v $(pwd):/app composer/composer install
+
+echo "YEAH! UP AND RUNNING"
